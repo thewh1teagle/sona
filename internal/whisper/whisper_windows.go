b4e95e18f176916ff5032d6ece7c971420df596e
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2026-02-04 03:30:53 +0200

    static linking on windows

--- internal/whisper/whisper_windows.go
+++ internal/whisper/whisper_windows.go
@@ -6,8 +6,8 @@ package whisper
 #cgo CFLAGS: -I${SRCDIR}/../../third_party/include
 #cgo LDFLAGS: -L${SRCDIR}/../../third_party/lib
 #cgo LDFLAGS: -lwhisper -lggml -lggml-base -lggml-cpu -lggml-vulkan
-#cgo LDFLAGS: -lvulkan-1 -lstdc++ -lm -lpthread -lgomp
-#cgo LDFLAGS: -static-libstdc++ -static-libgcc
+#cgo LDFLAGS: -lvulkan-1 -lm
+#cgo LDFLAGS: -Wl,-Bstatic -lstdc++ -lgomp -lwinpthread -Wl,-Bdynamic
 #include <whisper.h>
 #include <stdlib.h>
 */
